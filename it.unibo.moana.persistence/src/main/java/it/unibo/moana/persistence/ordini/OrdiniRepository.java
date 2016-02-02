9d330612d2a149ae31f09ba018c736f52274c5d2
Thomas Farneti <thomas.farneti@gmail.com>
2016-02-02 17:20:32 +0100

    Add basic cnp

--- it.unibo.moana.persistence/src/main/java/it/unibo/moana/persistence/ordini/OrdiniRepository.java
+++ it.unibo.moana.persistence/src/main/java/it/unibo/moana/persistence/ordini/OrdiniRepository.java
@@ -1,13 +1,7 @@
 package it.unibo.moana.persistence.ordini;
 
 import it.unibo.moana.core.domain.Ordini.IOrdineRepository;
-import it.unibo.moana.core.domain.Ordini.Ordine;
-import it.unibo.moana.core.infrastructure.persistence.IRepository;
 
 public class OrdiniRepository implements IOrdineRepository {
-	protected IRepository<Ordine, String> repo;
-	
-	public OrdiniRepository(IRepository<Ordine, String> repo){
-		this.repo = repo;
-	}
+
 }
